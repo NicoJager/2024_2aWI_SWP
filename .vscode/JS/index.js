97e57edbaadad06a398f0ef2ab80a5460f478909
NicoJager <nico.jaeger@student.htkdornbirn.at>
2025-02-07 14:48:26 +0100

    Ende der Stunde

---
+++ .vscode/JS/index.js
@@ -0,0 +1,71 @@
+{
+    console.log("Hello World - How are you?");
+    let a = 10;
+    let b = 20;
+    let result = a + b;
+
+    console.log(result);
+
+    // k=3; d=2
+    // f(x)= k*x+d
+    //f(2)= 3*2+2 = 8 
+}
+
+
+let a = 7;
+let b = 3;
+
+let sum = a + b;
+
+let difference = a - b;
+
+let product = a * b;
+
+let quotient = a / b;
+
+let remainder = a % b;
+
+let power = a ** b;
+
+let expression = (a + b) * b;
+
+if (a == b) {
+    console.log("a is equal to b");
+}
+
+else if (a < b) {
+    console.log("a is bigger than b");
+
+}
+else {
+    console.log("a is bigger than b");
+
+}
+
+// equal to
+console.log(1 == 1);
+
+// not equal to
+console.log(2 == 1);
+
+// greater than
+console.log(2 > 1);
+
+// greater or equal than
+console.log(2 >= 1)
+
+let string1 = "LIAM MATT KREIGENBRINK";
+
+console.log(string1);
+
+// LOOOOOOOOOPSSS
+
+console.log("Nico")
+console.log("Nico")
+console.log("Nico")
+
+for (let i = 0; i < 10; i++) {
+    console.log("Nico")
+    console.log("Durchlauf: " + i)
+}
+
